1c52e710e941fa59a568fab80b80d1845e0e9d88
zhangsen1996 <497801714@qq.com>
2021-09-30 17:14:38 +0800

    第三次提交

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.zhangsen.dao")
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        System.out.println();
     }
 }
